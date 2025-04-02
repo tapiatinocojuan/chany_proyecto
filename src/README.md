e7d8aab4746d8e3044e8668a3946ad7a25f2faf9
Juan Tapia Tinoco <tapiatinocojuan@gmail.com>
2025-04-02 01:18:14 -0600

    Se realizan modificaciones a los colores de la interfaz y se agrega funcionalidad para cambiar valor de slider mediante el textCtrl

---
+++ src/README.md
@@ -0,0 +1,20 @@
+# Gracias por utilizar nuestro software
+
+Esta interfaz fue creada con la intención de facilitar el trabajo en nuestro taller
+de personalización. Sirve para separar un grande en multiples páginas tamaño carta, oficio
+o A4.
+
+Nosotros lo utilizamos para crear mosaicos de hojas A4 y así poder sublimar diseños más
+grandes que lo que nos permite imprimir nuestro equipo actual.
+
+Solo requiere de wxpython y pillow adicionales a la librería estandar de python. Esperamos
+que pueda ser de utilidad este código para personas que como nosotros nos dedicamos a la
+personalización de productos sublimados.
+
+También tenemos una versión para instalar en windows, si no quieres complicarte con la
+instalación de dependencias esa es una buena opción, la contraseña de instalación es 
+"sandink".
+
+Hemos notado que en ocasiones al exportar a pdf aparece un recuadro negro de fondo en las
+imagenes, aún no sabemos que es lo que causa este efecto adverso pero al exportar a png
+el mismo archivo este inconveniente desaparece.
--- src/chany_proyecto.py
+++ src/chany_proyecto.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ###########################################################################
-## Python code generated with wxFormBuilder (version 4.0.0-0-g0efcecf)
+## Python code generated with wxFormBuilder (version 4.2.1-0-g80c4cb6)
 ## http://www.wxformbuilder.org/
 ##
 ## PLEASE DO *NOT* EDIT THIS FILE!
@@ -17,9 +17,10 @@ import wx.xrc
 class ChanyFrame ( wx.Frame ):
 
 	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 876,633 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Exportar a mosaico", pos = wx.DefaultPosition, size = wx.Size( 1117,843 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
 		self.SetSizeHints( wx.Size( 876,633 ), wx.DefaultSize )
+		self.SetFont( wx.Font( 9, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Balbaleo" ) )
 
 		bSizer1 = wx.BoxSizer( wx.VERTICAL )
 
@@ -27,9 +28,12 @@ class ChanyFrame ( wx.Frame ):
 		self.m_splitter1.Bind( wx.EVT_IDLE, self.m_splitter1OnIdle )
 
 		self.m_panel31 = wx.Panel( self.m_splitter1, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		self.m_panel31.SetFont( wx.Font( 12, wx.FONTFAMILY_ROMAN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Clarendon BT" ) )
+		self.m_panel31.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOW ) )
+
 		bSizer2 = wx.BoxSizer( wx.VERTICAL )
 
-		sbSizer1 = wx.StaticBoxSizer( wx.StaticBox( self.m_panel31, wx.ID_ANY, u"archivo" ), wx.VERTICAL )
+		sbSizer1 = wx.StaticBoxSizer( wx.StaticBox( self.m_panel31, wx.ID_ANY, u"Cargar archivo" ), wx.VERTICAL )
 
 		self.m_filePicker1 = wx.FilePickerCtrl( sbSizer1.GetStaticBox(), wx.ID_ANY, wx.EmptyString, u"Select a file", u"*.*", wx.DefaultPosition, wx.DefaultSize, wx.FLP_DEFAULT_STYLE )
 		sbSizer1.Add( self.m_filePicker1, 0, wx.ALL|wx.EXPAND, 5 )
@@ -43,16 +47,16 @@ class ChanyFrame ( wx.Frame ):
 		bSizer2.Add( self.m_radioBox1, 0, wx.ALL|wx.EXPAND, 5 )
 
 		m_radioBox11Choices = [ u"Retrato", u"Paisaje" ]
-		self.m_radioBox11 = wx.RadioBox( self.m_panel31, wx.ID_ANY, u"Direccion", wx.DefaultPosition, wx.DefaultSize, m_radioBox11Choices, 1, wx.RA_SPECIFY_ROWS )
-		self.m_radioBox11.SetSelection( 1 )
+		self.m_radioBox11 = wx.RadioBox( self.m_panel31, wx.ID_ANY, u"Orientación", wx.DefaultPosition, wx.DefaultSize, m_radioBox11Choices, 1, wx.RA_SPECIFY_ROWS )
+		self.m_radioBox11.SetSelection( 0 )
 		bSizer2.Add( self.m_radioBox11, 0, wx.ALL|wx.EXPAND, 5 )
 
-		sbSizer2211 = wx.StaticBoxSizer( wx.StaticBox( self.m_panel31, wx.ID_ANY, u"ancho linea roja" ), wx.HORIZONTAL )
+		sbSizer2211 = wx.StaticBoxSizer( wx.StaticBox( self.m_panel31, wx.ID_ANY, u"Ancho linea roja" ), wx.HORIZONTAL )
 
-		self.m_slider1211 = wx.Slider( sbSizer2211.GetStaticBox(), wx.ID_ANY, 3, 1, 10, wx.DefaultPosition, wx.DefaultSize, wx.SL_HORIZONTAL )
+		self.m_slider1211 = wx.Slider( sbSizer2211.GetStaticBox(), wx.ID_ANY, 3, 1, 10, wx.DefaultPosition, wx.DefaultSize, wx.SL_AUTOTICKS|wx.SL_HORIZONTAL|wx.SL_LABELS )
 		sbSizer2211.Add( self.m_slider1211, 5, wx.ALL|wx.EXPAND, 1 )
 
-		self.m_textCtrl2112 = wx.TextCtrl( sbSizer2211.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 50,-1 ), 0 )
+		self.m_textCtrl2112 = wx.TextCtrl( sbSizer2211.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 50,-1 ), wx.TE_PROCESS_ENTER )
 		sbSizer2211.Add( self.m_textCtrl2112, 0, wx.ALL, 5 )
 
 
@@ -60,10 +64,10 @@ class ChanyFrame ( wx.Frame ):
 
 		sbSizer221 = wx.StaticBoxSizer( wx.StaticBox( self.m_panel31, wx.ID_ANY, u"x_off" ), wx.HORIZONTAL )
 
-		self.m_slider121 = wx.Slider( sbSizer221.GetStaticBox(), wx.ID_ANY, 0, 0, 10, wx.DefaultPosition, wx.DefaultSize, wx.SL_HORIZONTAL )
+		self.m_slider121 = wx.Slider( sbSizer221.GetStaticBox(), wx.ID_ANY, 0, 0, 10, wx.DefaultPosition, wx.DefaultSize, wx.SL_AUTOTICKS|wx.SL_HORIZONTAL|wx.SL_LABELS )
 		sbSizer221.Add( self.m_slider121, 5, wx.ALL|wx.EXPAND, 1 )
 
-		self.m_textCtrl211 = wx.TextCtrl( sbSizer221.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 50,-1 ), 0 )
+		self.m_textCtrl211 = wx.TextCtrl( sbSizer221.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 50,-1 ), wx.TE_PROCESS_ENTER )
 		sbSizer221.Add( self.m_textCtrl211, 0, wx.ALL, 5 )
 
 
@@ -71,32 +75,32 @@ class ChanyFrame ( wx.Frame ):
 
 		sbSizer22 = wx.StaticBoxSizer( wx.StaticBox( self.m_panel31, wx.ID_ANY, u"y_off" ), wx.HORIZONTAL )
 
-		self.m_slider12 = wx.Slider( sbSizer22.GetStaticBox(), wx.ID_ANY, 0, 0, 10, wx.DefaultPosition, wx.DefaultSize, wx.SL_HORIZONTAL )
+		self.m_slider12 = wx.Slider( sbSizer22.GetStaticBox(), wx.ID_ANY, 0, 0, 10, wx.DefaultPosition, wx.DefaultSize, wx.SL_AUTOTICKS|wx.SL_HORIZONTAL|wx.SL_LABELS )
 		sbSizer22.Add( self.m_slider12, 5, wx.ALL|wx.EXPAND, 1 )
 
-		self.m_textCtrl2111 = wx.TextCtrl( sbSizer22.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 50,-1 ), 0 )
+		self.m_textCtrl2111 = wx.TextCtrl( sbSizer22.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 50,-1 ), wx.TE_PROCESS_ENTER )
 		sbSizer22.Add( self.m_textCtrl2111, 0, wx.ALL, 5 )
 
 
 		bSizer2.Add( sbSizer22, 0, wx.EXPAND, 5 )
 
-		sbSizer2 = wx.StaticBoxSizer( wx.StaticBox( self.m_panel31, wx.ID_ANY, u"translape" ), wx.HORIZONTAL )
+		sbSizer2 = wx.StaticBoxSizer( wx.StaticBox( self.m_panel31, wx.ID_ANY, u"Translape" ), wx.HORIZONTAL )
 
-		self.m_slider1 = wx.Slider( sbSizer2.GetStaticBox(), wx.ID_ANY, 0, 0, 10, wx.DefaultPosition, wx.DefaultSize, wx.SL_HORIZONTAL )
+		self.m_slider1 = wx.Slider( sbSizer2.GetStaticBox(), wx.ID_ANY, 0, 0, 10, wx.DefaultPosition, wx.DefaultSize, wx.SL_AUTOTICKS|wx.SL_HORIZONTAL|wx.SL_LABELS )
 		sbSizer2.Add( self.m_slider1, 5, wx.ALL|wx.EXPAND, 1 )
 
-		self.m_textCtrl2 = wx.TextCtrl( sbSizer2.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 50,-1 ), 0 )
+		self.m_textCtrl2 = wx.TextCtrl( sbSizer2.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 50,-1 ), wx.TE_PROCESS_ENTER )
 		sbSizer2.Add( self.m_textCtrl2, 0, wx.ALL, 5 )
 
 
 		bSizer2.Add( sbSizer2, 0, wx.EXPAND, 5 )
 
-		sbSizer21 = wx.StaticBoxSizer( wx.StaticBox( self.m_panel31, wx.ID_ANY, u"margen" ), wx.HORIZONTAL )
+		sbSizer21 = wx.StaticBoxSizer( wx.StaticBox( self.m_panel31, wx.ID_ANY, u"Margen" ), wx.HORIZONTAL )
 
-		self.m_slider11 = wx.Slider( sbSizer21.GetStaticBox(), wx.ID_ANY, 0, 0, 10, wx.DefaultPosition, wx.DefaultSize, wx.SL_HORIZONTAL )
+		self.m_slider11 = wx.Slider( sbSizer21.GetStaticBox(), wx.ID_ANY, 0, 0, 10, wx.DefaultPosition, wx.DefaultSize, wx.SL_AUTOTICKS|wx.SL_HORIZONTAL|wx.SL_LABELS )
 		sbSizer21.Add( self.m_slider11, 1, wx.ALL|wx.EXPAND, 5 )
 
-		self.m_textCtrl1 = wx.TextCtrl( sbSizer21.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 50,-1 ), 0 )
+		self.m_textCtrl1 = wx.TextCtrl( sbSizer21.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 50,-1 ), wx.TE_PROCESS_ENTER )
 		sbSizer21.Add( self.m_textCtrl1, 0, wx.ALL, 5 )
 
 
@@ -104,16 +108,16 @@ class ChanyFrame ( wx.Frame ):
 
 		sbSizer211 = wx.StaticBoxSizer( wx.StaticBox( self.m_panel31, wx.ID_ANY, u"DPI" ), wx.HORIZONTAL )
 
-		self.m_slider111 = wx.Slider( sbSizer211.GetStaticBox(), wx.ID_ANY, 300, 72, 300, wx.DefaultPosition, wx.DefaultSize, wx.SL_HORIZONTAL )
+		self.m_slider111 = wx.Slider( sbSizer211.GetStaticBox(), wx.ID_ANY, 300, 72, 300, wx.DefaultPosition, wx.DefaultSize, wx.SL_AUTOTICKS|wx.SL_HORIZONTAL|wx.SL_LABELS )
 		sbSizer211.Add( self.m_slider111, 1, wx.ALL|wx.EXPAND, 5 )
 
-		self.m_textCtrl11 = wx.TextCtrl( sbSizer211.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 50,-1 ), 0 )
+		self.m_textCtrl11 = wx.TextCtrl( sbSizer211.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 50,-1 ), wx.TE_PROCESS_ENTER )
 		sbSizer211.Add( self.m_textCtrl11, 0, wx.ALL, 5 )
 
 
 		bSizer2.Add( sbSizer211, 0, wx.EXPAND, 5 )
 
-		sbSizer3 = wx.StaticBoxSizer( wx.StaticBox( self.m_panel31, wx.ID_ANY, u"exportar archivo" ), wx.VERTICAL )
+		sbSizer3 = wx.StaticBoxSizer( wx.StaticBox( self.m_panel31, wx.ID_ANY, u"Exportar archivo" ), wx.VERTICAL )
 
 		bSizer5 = wx.BoxSizer( wx.HORIZONTAL )
 
@@ -122,7 +126,7 @@ class ChanyFrame ( wx.Frame ):
 
 		m_radioBox12Choices = [ u"PDF", u"PNG" ]
 		self.m_radioBox12 = wx.RadioBox( sbSizer3.GetStaticBox(), wx.ID_ANY, u"Tipo archivo", wx.DefaultPosition, wx.DefaultSize, m_radioBox12Choices, 1, wx.RA_SPECIFY_ROWS )
-		self.m_radioBox12.SetSelection( 1 )
+		self.m_radioBox12.SetSelection( 0 )
 		bSizer5.Add( self.m_radioBox12, 1, wx.ALL, 5 )
 
 
@@ -139,6 +143,8 @@ class ChanyFrame ( wx.Frame ):
 		bSizer3 = wx.BoxSizer( wx.VERTICAL )
 
 		self.m_panel2 = wx.Panel( self.m_panel3, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		self.m_panel2.SetFont( wx.Font( 9, wx.FONTFAMILY_ROMAN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Clarendon Blk BT" ) )
+
 		bSizer3.Add( self.m_panel2, 1, wx.EXPAND |wx.ALL, 5 )
 
 		self.m_slider6 = wx.Slider( self.m_panel3, wx.ID_ANY, 100, 10, 100, wx.DefaultPosition, wx.DefaultSize, wx.SL_HORIZONTAL )
@@ -148,7 +154,7 @@ class ChanyFrame ( wx.Frame ):
 		self.m_panel3.SetSizer( bSizer3 )
 		self.m_panel3.Layout()
 		bSizer3.Fit( self.m_panel3 )
-		self.m_splitter1.SplitVertically( self.m_panel31, self.m_panel3, 0 )
+		self.m_splitter1.SplitVertically( self.m_panel31, self.m_panel3, 394 )
 		bSizer1.Add( self.m_splitter1, 1, wx.EXPAND, 5 )
 
 
@@ -161,6 +167,7 @@ class ChanyFrame ( wx.Frame ):
 		self.Bind( wx.EVT_SIZE, self.on_change_size )
 		self.m_splitter1.Bind( wx.EVT_SPLITTER_SASH_POS_CHANGED, self.on_sash_changed )
 		self.m_filePicker1.Bind( wx.EVT_FILEPICKER_CHANGED, self.on_file_change )
+		self.m_textCtrl2112.Bind( wx.EVT_TEXT_ENTER, self.bfbdf )
 		self.m_filePicker2.Bind( wx.EVT_FILEPICKER_CHANGED, self.on_separar )
 		self.m_panel2.Bind( wx.EVT_PAINT, self.on_paint )
 		self.m_slider6.Bind( wx.EVT_SLIDER, self.on_change_size )
@@ -179,6 +186,9 @@ class ChanyFrame ( wx.Frame ):
 	def on_file_change( self, event ):
 		event.Skip()
 
+	def bfbdf( self, event ):
+		event.Skip()
+
 	def on_separar( self, event ):
 		event.Skip()
 
@@ -187,7 +197,7 @@ class ChanyFrame ( wx.Frame ):
 
 
 	def m_splitter1OnIdle( self, event ):
-		self.m_splitter1.SetSashPosition( 0 )
+		self.m_splitter1.SetSashPosition( 394 )
 		self.m_splitter1.Unbind( wx.EVT_IDLE )
 
 
